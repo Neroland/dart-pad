7276a154b48137f09053f496332c46fde17a4c59
Dario Maselli <117168592+Dario-Maselli@users.noreply.github.com>
2025-03-22 20:56:10 +0200

    Add support for flutter on windows

--- pkgs/dart_services/lib/src/sdk.dart
+++ pkgs/dart_services/lib/src/sdk.dart
@@ -1,7 +1,3 @@
-// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'dart:convert';
 import 'dart:io';
 
@@ -87,7 +83,11 @@ final class Sdk {
     }
 
     final flutterBinPath = path.join(flutterSdkPath, 'bin');
-    final flutterToolPath = path.join(flutterBinPath, 'flutter');
+    // On Windows use flutter.bat, otherwise use the standard flutter executable.
+    final flutterToolPath = path.join(
+      flutterBinPath,
+      Platform.isWindows ? 'flutter.bat' : 'flutter',
+    );
     final dartSdkPath = path.join(flutterSdkPath, 'bin', 'cache', 'dart-sdk');
     final dartVersion = _readDartSdkVersionFile(dartSdkPath);
 
